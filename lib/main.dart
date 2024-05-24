8e6212fdafa4e50473ce9516daf9ceabac54181b
Malinda Prabath <malindaprabath876@gmail.com>
2024-05-24 15:44:29 +0530

    files created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apple/screens/spash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,11 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Apple Store",
+      home: SplashScreen(),
+    );
   }
 }
-
-
